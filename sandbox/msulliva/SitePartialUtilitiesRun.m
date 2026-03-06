80a46cc83a368f50daf4edb49663bbcb68d08d8f
Sankhanil <sankhanil@gmail.com>
2026-03-06 06:57:23 -0500

    sandbox runscripts

--- sandbox/msulliva/SitePartialUtilitiesRun.m
+++ sandbox/msulliva/SitePartialUtilitiesRun.m
@@ -1,27 +1,37 @@
-%% Site Full Run Example (Setup, HEA, and L2)
+%% Site Partial Utilities Run Example (Setup, HEA, and L2)
 % Michael Sullivan
 % 5/6/2025
 
 %% Prepare Input Data and Files for HEA
 % Use the new schema for test data loading from the test file.
 buildingDataPath = fullfile(ecetest.testDataRoot,...
-    "buildingInputs.xlsx");
+    "singleBuildingInputs.xlsx");
 utilityDataPath = fullfile(ecetest.testDataRoot,...
-    "utilityInputs1.xlsx");
+    "partialUtilityInputs.xlsx");
 hddDataPath = fullfile(ecetest.testDataRoot,...
     "historicalDDInputs.xlsx");
-weatherDataPath = fullfile(ecetest.testDataRoot,...
-    "weatherDataInputs.xlsx");
-siteInputsDataPath = fullfile(ecetest.testDataRoot,...
-    "siteInputs.xlsx");
 
 %% Initialize Site Object
 % Set up Site from optional inputs.
 site = ece.Site.fromInputExcelFiles(...
     "BuildingPath",buildingDataPath,...
     "UtilityPath",utilityDataPath,...
-    "HistDDPath",hddDataPath,...
-    "WeatherDataPath",weatherDataPath,"SiteInputsDataPath",siteInputsDataPath);
+    "HistDDPath",hddDataPath);
+
+%% KLUDGE: Force Buildings to Exist in Boston to Connect with Solar API
+% This should be done more elegantly with a Location class.
+for bldgIdx = 1:site.NumBuildings
+    % Extract Building
+    bldg = site.Buildings(bldgIdx);
+    
+    % Set Location City/State for API
+    bldg.LocationCity = "Boston";
+    bldg.LocationState = "MA";
+    
+    % Map Location to Lat/Long
+    bldg.matchCityLocation();
+
+end %forloop
 
 %% Run Processing Routines
 % Process each utility and then generate Buildings' corresponding Annual
@@ -38,7 +48,6 @@ site.computeHEA();
 buildingLevel2DataPath = fullfile(ecetest.testDataRoot,...
     "buildingL2Inputs.xlsx");
 
-
 %% Load L2 Data into Single Building on Site
 % Level 2 calculation will be done on one specific building within Site at
 % a time.
@@ -50,7 +59,7 @@ site.Buildings(1).importLevel2ObjectData(buildingLevel2DataPath);
 site.Buildings(1).computeLevel2();
 
 % Display result to command line.
-disp(site.Buildings(1).Level2);
+disp(site.Buildings(1).RunStatsTable);
 
 
 
